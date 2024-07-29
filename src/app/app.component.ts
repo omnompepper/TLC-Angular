53dfa458a271684a22c229789fc4a66cdc1bf9b9
Claire Thomas <claire.thomas@matraining.com>
2024-07-29 11:36:19 +0100

    Day 1 work commited - added a counter component, small bit of binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CounterComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'demo';
+  name = 'Clairebear';
 }
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+
+export class CounterComponent {
+  count = 0;
+
+  increment() {
+    this.count++;
+  }
+}
