cf78322b0607399cff3615c10a11bc75333ed545
Claire Thomas <claire.thomas@matraining.com>
2024-08-01 10:29:04 +0100

    Tables added for accounts including sorting

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -1,26 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { UpperCasePipe } from '@angular/common';
-import { NgFor } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { NgIf } from '@angular/common';
-import { Observable, of } from 'rxjs';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule, Sort } from '@angular/material/sort';
 
 import { Account } from '../models/account';
 import { AccountService } from '../services/account.service';
-import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-accounts',
   standalone: true,
-  imports: [UpperCasePipe, NgFor, NgIf, FormsModule],
+  imports: [MatTableModule,
+    MatSortModule],
   templateUrl: './accounts.component.html',
   styleUrl: './accounts.component.css'
 })
 export class AccountsComponent implements OnInit{
+  public displayedColumns: string[] = ['id', 'name', 'balance'];
+  
   accounts: Account[] = [];
 
-  constructor(private accountService: AccountService,
-    private messageService: MessageService) {}
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
     this.getAccounts();
@@ -28,7 +27,35 @@ export class AccountsComponent implements OnInit{
 
   getAccounts(): void {
     this.accountService.getAccounts()
-    .subscribe((accounts: any) => this.accounts = accounts);
+    .subscribe((accounts: any) => {
+      this.accounts = accounts;
+    });
   }
 
+  sortAccountData(sort: Sort) {
+    const data = this.accounts.slice();
+    if (!sort.active || sort.direction === '') {
+      this.accounts = data;
+      return;
+    }
+
+    this.accounts = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'name':
+          return compare(a.name, b.name, isAsc);
+        case 'id':
+          return compare(a.id, b.id, isAsc);
+        case 'balance':
+          return compare(a.balance, b.balance, isAsc);
+        default:
+          console.log("default");
+          return 0;
+      }
+    });
+  }
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
