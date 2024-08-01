dca06e10751bfbfa18b1203258962cebb472f203
Claire Thomas <claire.thomas@matraining.com>
2024-08-01 14:25:11 +0100

    Got clairetest harness working with matsort yaygit add .!

--- src/app/clairetest/clairetest.component.ts
+++ src/app/clairetest/clairetest.component.ts
@@ -1,23 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
 import { MatCardModule } from '@angular/material/card';
-import { MatSortModule, Sort } from '@angular/material/sort';
+import { MatSortModule, MatSort, Sort } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
+
+import { ViewChild } from '@angular/core';
 
 import { Account } from '../models/account';
+
+const ACCOUNT_DATA: Account[] = [
+  { id: 1, name: 'Alice', balance: 10 },
+  { id: 2, name: 'Bob', balance: 200 },
+];
 
 @Component({
   selector: 'app-clairetest',
   standalone: true,
-  imports: [MatCardModule, MatSortModule, MatTableModule],
+  imports: [MatCardModule, MatSortModule, MatTableModule, MatSort],
   templateUrl: './clairetest.component.html',
-  styleUrl: './clairetest.component.css'
+  styleUrl: './clairetest.component.css',
 })
-export class ClairetestComponent implements OnInit{
+export class ClairetestComponent implements OnInit {
   accounts: Account[] = [];
   public displayedColumns: string[] = ['id', 'name', 'balance'];
+  public datasource = new MatTableDataSource(ACCOUNT_DATA);
 
-  
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.datasource.sort = this.sort;
+  }
+
   ngOnInit(): void {
     this.getAccounts();
   }
@@ -27,8 +41,8 @@ export class ClairetestComponent implements OnInit{
     // however we're just going to get some dummy data for now
     // for testing tables
     this.accounts = [
-      {id: 1, name: "Alice", balance: 10},
-      {id:2, name: "Bob", balance: 200}
+      { id: 1, name: 'Alice', balance: 10 },
+      { id: 2, name: 'Bob', balance: 200 },
     ];
   }
 }
