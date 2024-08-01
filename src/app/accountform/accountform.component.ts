7cdd36150c81530e038b8323c7c72469bf89664a
Claire Thomas <claire.thomas@matraining.com>
2024-08-01 11:05:59 +0100

    More cleanup of existing code, mainly account form

--- src/app/accountform/accountform.component.ts
+++ src/app/accountform/accountform.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
 import { Account } from '../models/account';
 import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-accountform',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule],
   templateUrl: './accountform.component.html',
-  styleUrl: './accountform.component.css'
+  styleUrl: './accountform.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AccountformComponent {
   
-  constructor( private accountService: AccountService){ 
-  }
+  constructor( private accountService: AccountService, private router:Router){ }
 
   account = new Account(10,"",0);
   error?: string;
@@ -21,9 +32,16 @@ export class AccountformComponent {
   onSubmit(){
     this.accountService.saveAccount(this.account).subscribe({
       next: value => {
-        //this.account = new Account(0,"",0);
+        this.account = new Account(0,"",0);
+        this.router.navigate(['accounts']);
       },
       error: error => this.error = error
     });
+  }
+
+  protected readonly value = signal('');
+
+  protected onInput(event: Event) {
+    this.value.set((event.target as HTMLInputElement).value);
   }
 }
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { MessageService } from '../services/message.service';
 import { NgFor, NgForOf } from '@angular/common';
-import { MatButton, MatButtonModule } from '@angular/material/button';
-
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [NgFor, NgForOf, MatButton, MatButtonModule],
+  imports: [NgFor, NgForOf, MatButtonModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
